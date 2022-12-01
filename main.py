83d3c403810a82d000860c299edbbead8d6902a4
Justin Fields <justinlime1999@gmail.com>
2022-11-30 19:52:10 -0600

    Added 24 Hour Percentage Change

--- main.py
+++ main.py
@@ -4,36 +4,35 @@ import json
 import os
 from discord.ext import commands, tasks
 
-intents = discord.Intents.default()
-bot = commands.Bot(intents=intents)
-url = "https://api.coingecko.com/api/v3/simple/price"
-
 with open('info.json', 'r') as u:
     info = json.load(u)
     api = info['api']
-    coin = info['coin']
     id = info['id']
     length = info['length']
     updatefreq = info['updatefreq']
     print('info loaded')
 
-params = {
-    'ids' : id,
-    'vs_currencies' : "usd"
-}
+intents = discord.Intents.default()
+bot = commands.Bot(intents=intents)
+url = f"https://api.coingecko.com/api/v3/coins/{id}"
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{coin} Price"))
     change_status.start()
     print("BOT RUNNING")
 
 @tasks.loop(seconds=updatefreq)
 async def change_status():
-    price = str(requests.get(url,params=params).json()[id]['usd'])[0:length]
-    print(price)
+    data = requests.get(url).json()
+    price = str(data['market_data']['current_price']['usd'])[0:length]
+    change = str(data['market_data']['price_change_percentage_24h'])
+    if change[0] != '-':
+        change = f'+{change}'
+    change = change[0:6]
     for guild in bot.guilds:
         await guild.me.edit(nick=price)
-    print(f"updated {coin}")
-
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, 
+    name=f"24h: {change}%"))
+    print(f"updated {id} with price {price} and 24h percentage change {change}")
+    
 bot.run(api)
